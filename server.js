8b75ff5d61f9db5cef08bc267c8962b3209bdca1
Andres Leal <pixele9@gmail.com>
2021-02-22 18:01:20 -0600

    feat: upload redirect to /

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const storage = multer.diskStorage({
 
 const app = express();
 
+app.set("views", path.join(__dirname, "public"))
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -31,13 +33,32 @@ app.use(multer({
     storage
 }).single("image"))
 
-app.post("/upload", (req, res) => {
-    //fs.readFile('image.png', (err, data) => {
-    console.log("images/image.png", req.file)
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.get("/upload", (req, res) => {
+    socket.on("clientImgConversion", data => {
+        console.log("DATA:: ", data)
+        io.sockets.emit("serverImgConversion", data)
+    })
+
     io.on("connection", socket => {
-        fs.readFile("images/image.png", (err, data) => {
+        fs.readFile(req.file.path, (err, data) => {
             socket.emit('clientImgConversion', { image: true, buffer: data });
             socket.emit('serverImgConversion', "data:image/png;base64,"+ data.toString("base64"));
         });
     })
+})
+
+app.post("/upload", (req, res) => {
+    //fs.readFile('image.png', (err, data) => {
+    console.log(req.file.path, req.file)
+    io.on("connection", socket => {
+        fs.readFile(req.file.path, (err, data) => {
+            socket.emit('clientImgConversion', { image: true, buffer: data });
+            socket.emit('serverImgConversion', "data:image/png;base64,"+ data.toString("base64"));
+        });
+    })
+    res.redirect("/")
 })
